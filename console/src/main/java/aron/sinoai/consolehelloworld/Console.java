e323155085ba6bd96fe7c146be48d77b6e2d978e
asinoai <aron.sinoai@linkonomics.com>
2016-05-09 12:22:02 +0300

    Custom anonymous printer, Ver 2

--- console/src/main/java/aron/sinoai/consolehelloworld/Console.java
+++ console/src/main/java/aron/sinoai/consolehelloworld/Console.java
@@ -2,6 +2,7 @@ package aron.sinoai.consolehelloworld;
 
 public class Console  {
 
+
     public static void main(String[] args) {
         System.out.println("Start!");
 
@@ -14,5 +15,17 @@ public class Console  {
         final IHelloPrinter anotherPrinter = new HelloMoonPrinter(1);
         anotherPrinter.print();
         anotherPrinter.print();
+
+        final IHelloPrinter customPrinter = new IHelloPrinter() {
+            @Override
+            public void print() {
+                System.out.println("Hello, Custom anonymous!");
+            }
+        };
+
+        customPrinter.print();
+        customPrinter.print();
+
     }
+
 }
